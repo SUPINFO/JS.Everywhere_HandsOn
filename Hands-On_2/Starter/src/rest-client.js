ac62189856b5a7dd5cd9e5e167e76e6c04d0a83c
bargenson <bargenson@gmail.com>
2012-11-15 23:11:24 +0100

    Slides + examples for Hands-on 2

--- Hands-On_2/Starter/src/rest-client.js
+++ Hands-On_2/Starter/src/rest-client.js
@@ -1,3 +1,11 @@
 var HOST = "http://sheltered-oasis-7703.herokuapp.com";
 
-var restClient = {};
+var restClient = {
+
+	property: "This is a property",
+
+	method: function() {
+		console.log("This is a method");
+	}
+
+};
